e11d47cea681b03c195c936f37f72232d25d20b1
pablo <pabloocampopita@gmail.com>
2019-06-25 10:54:28 -0400

    my first packages

---
+++ index.js
@@ -0,0 +1,12 @@
+const pabloca = require('pabloca');
+const crunch = require('crunchyholymonkey');
+const domina = require('dominantgender');
+const krazy = require('krazymonkey45');
+const incre = require('incredibilis');
+
+pabloca.helloWorld();
+crunch.byebyeworld();
+domina.helloWorld();
+krazy.helloWorld();
+incre.helloWorld();
+
